452a8e0bd748c72a806fc9ba175f84d1d308dd25
mariamelwirish <mariamelwirish@gmail.com>
2026-04-05 17:46:31 -0400

    using same seed to be reproducable

--- environment.py
+++ environment.py
@@ -75,7 +75,7 @@ class VM:
     
 
 class CloudEnvironment:
-    def __init__(self, n_vms, n_jobs, io_vm_ratio, io_job_ratio, mean_job_arrival_rate, mean_job_length, variance_job_length, ):
+    def __init__(self, n_vms, n_jobs, io_vm_ratio, io_job_ratio, mean_job_arrival_rate, mean_job_length, variance_job_length, seed,):
         self.n_vms = n_vms
         self.n_jobs = n_jobs
         self.io_vm_ratio = io_vm_ratio
@@ -83,6 +83,7 @@ class CloudEnvironment:
         self.mean_job_arrival_rate = mean_job_arrival_rate
         self.mean_job_length = mean_job_length
         self.variance_job_length = variance_job_length
+        self.seed = seed
 
         self.vms = self.create_vms()
         self.jobs = self.generate_jobs()
@@ -91,39 +92,36 @@ class CloudEnvironment:
 
     def create_vms(self) -> list[VM]:
         vms = []
-        np.random.seed(42)
         # create exactly the right number of each type
         n_io_vms = int(self.n_vms * self.io_vm_ratio)
         types = ['I/O'] * n_io_vms + ['CPU'] * (self.n_vms - n_io_vms)
-        np.random.shuffle(types)  # shuffle so they're not all I/O first
+        self.seed.shuffle(types)  # shuffle so they're not all I/O first
 
         for i in range(self.n_vms):
             vm_type = types[i]
-            v_com = np.random.uniform(1000, 3000)
-            v_cost = np.random.uniform(4, 5)
+            v_com = self.seed.uniform(1000, 3000)
+            v_cost = self.seed.uniform(4, 5)
             vms.append(VM(i, v_com, vm_type, v_cost))
         
         return vms
 
     def generate_jobs(self):
-        np.random.seed(42)
         jobs = []
         current_time = 0
 
         # create exactly the right number of each type
         n_io_jobs = int(self.n_jobs * self.io_job_ratio)
         types = ['I/O'] * n_io_jobs + ['CPU'] * (self.n_jobs - n_io_jobs)
-        np.random.shuffle(types)  # shuffle so they're not all I/O first
+        self.seed.shuffle(types)  # shuffle so they're not all I/O first
 
         for i in range(self.n_jobs):
             job_type = types[i]
-            inter_arrival = np.random.exponential(1 / self.mean_job_arrival_rate)
+            inter_arrival = self.seed.exponential(1 / self.mean_job_arrival_rate)
             current_time += inter_arrival
             arrival_time = current_time            
-            req_com = max(1, np.random.normal(self.mean_job_length, np.sqrt(self.variance_job_length)))
-            # qos = np.random.uniform(3, 10) 
-            exe_time_estimate = req_com / 2000 # assuming average VM speed
-            qos = exe_time_estimate * np.random.uniform(2, 4)  # 10x-50x execution time
+            req_com = max(1, self.seed.normal(self.mean_job_length, np.sqrt(self.variance_job_length)))
+            exe_time_estimate = req_com / 2000 
+            qos = exe_time_estimate * self.seed.uniform(2, 4)  
             jobs.append(Job(i, arrival_time, req_com, job_type, qos))
         return jobs
     
