1c0362830b64b1f971a65c343b86f2b8b9718fea
mariamelwirish <mariamelwirish@gmail.com>
2026-05-18 11:56:17 +0300

    starting clean for the new system model

--- algorithms/random/scheduler.py
+++
@@ -1,6 +0,0 @@
-class RandomScheduler:
-    def __init__(self, seed):
-        self.seed = seed
-
-    def select_vm(self, vms):
-        return self.seed.choice(vms)
--- algorithms/round_robin/scheduler.py
+++
@@ -1,8 +0,0 @@
-class RoundRobinScheduler:
-    def __init__(self):
-        self.current_vm_index = 0
-
-    def select_vm(self, vms):
-        vm = vms[self.current_vm_index]
-        self.current_vm_index = (self.current_vm_index + 1) % len(vms)
-        return vm
